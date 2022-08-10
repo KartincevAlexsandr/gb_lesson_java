ebef7789757893348d7e5ca869905a2e7e64ec1d
Alexsandr Kart <sakart47@mail.ru>
2022-08-10 14:56:34 +0300

    add dz1

---
+++ src/dz1/DuplicateZeros.java
@@ -0,0 +1,16 @@
+package dz1;
+
+public class DuplicateZeros {
+    public void duplicateZeros(int[] arr) {
+        int[] newArr = arr.clone();
+
+        for(int i =0 , j = 0; i< newArr.length; i++,j++){
+            arr[i] = newArr[j];
+            if(newArr[j] == 0){
+                i++;
+                arr[i] = newArr[j];
+            }
+        }
+        return ;
+    }
+}
